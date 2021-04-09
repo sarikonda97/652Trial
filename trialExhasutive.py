2eade0afce9abdc467845dabbefa46ee47182dae
sarikonda97 <sarikonda97@gmail.com>
2021-04-08 21:30:56 -0600

    added all cocreativities

--- trialExhasutive.py
+++ trialExhasutive.py
@@ -2,6 +2,7 @@ import numpy as np
 import pickle
 import os, os.path
 import random
+import sys
 
 def check(a, b, upper_left):
     ul_row = upper_left[0]
@@ -34,12 +35,12 @@ def find_slice(big_array, small_array, complex, base, noOfSamples):
 def introduceCoCreativity(results, mode):
   if mode == "first":
     return results[0]
-  if mode == "last":
+  elif mode == "last":
     return results[-1]
-  if mode == "random":
+  elif mode == "random":
     return random.choice(results)
   # write something for max enemies
-  if mode == "maxEnemies":
+  elif mode == "maxEnemies":
     index = 0
     enemyCount = 0
     maxIndex = 0
@@ -57,6 +58,121 @@ def introduceCoCreativity(results, mode):
         maxIndex=index
       index+=1
     return results[maxIndex]
+  elif mode == "fifty-fifty":
+    index = 0
+    mostEqualIndex = 0
+    mostEqualDifference = sys.maxsize
+    KICount = 0
+    SMBCount = 0
+    KITiles = ['#', 'D', 'H', 'M', 'T']
+    SMBTiles = ['S', '?', 'Q', 'E', '<', '>', '[', ']', 'o', 'B', 'b']
+    for result in results:
+      resultArray = result.reshape(-1,).tolist()[0]
+      for i in range(0, len(resultArray)):
+        if resultArray[i] in KITiles:
+          KICount += 1
+        if resultArray[i] in SMBTiles:
+          SMBCount += 1
+      # targetProportionality = len(resultArray)/2
+      # if abs(targetProportionality - KICount) < abs(targetProportionality - SMBCount):
+      #   if abs(targetProportionality - KICount) < abs(targetProportionality - mostEqualCount):
+      #     mostEqualCount = KICount
+      #     mostEqualIndex = index
+      # else:
+      #   if abs(targetProportionality - SMBCount) < abs(targetProportionality - mostEqualCount):
+      #     mostEqualCount = SMBCount
+      #     mostEqualIndex = index
+      if abs(SMBCount - KICount) < mostEqualDifference:
+        mostEqualDifference = abs(SMBCount - KICount)
+        mostEqualIndex = index
+      index+=1
+      KICount = 0
+      SMBCount = 0
+    return results[mostEqualIndex]
+  elif mode == "maxUnique":
+    index=0
+    maxUniqueSize = 0
+    maxUniqueIndex = 0
+    uniqueSet = set()
+    for result in results:
+      resultArray = result.reshape(-1,).tolist()[0]
+      for resultElement in resultArray:
+        uniqueSet.add(resultElement)
+      if len(uniqueSet) > maxUniqueSize:
+        maxUniqueSize = len(uniqueSet)
+        maxUniqueIndex = index
+      uniqueSet.clear()
+      index+=1
+    return results[maxUniqueIndex]
+  elif mode == "minUnique":
+    index=0
+    minUniqueSize = sys.maxsize
+    minUniqueIndex = 0
+    uniqueSet = set()
+    for result in results:
+      resultArray = result.reshape(-1,).tolist()[0]
+      for resultElement in resultArray:
+        uniqueSet.add(resultElement)
+      if len(uniqueSet) < minUniqueSize:
+        minUniqueSize = len(uniqueSet)
+        minUniqueIndex = index
+      uniqueSet.clear()
+      index+=1
+    return results[minUniqueIndex]
+  elif mode == "maxKI":
+    index=0
+    KICount = 0
+    maxKICount = 0
+    maxKIIndex = 0
+    for result in results:
+      resultArray = result.reshape(-1,).tolist()[0]
+      KITiles = ['#', 'D', 'H', 'M', 'T']
+      for resultElement in resultArray:
+        if resultElement in KITiles:
+          KICount+=1
+      if KICount > maxKICount:
+        maxKICount = KICount
+        maxKIIndex = index
+      index += 1
+      KICount = 0
+    return results[maxKIIndex]
+  elif mode == "maxSMB":
+    index=0
+    SMBCount = 0
+    maxSMBCount = 0
+    maxSMBIndex = 0
+    for result in results:
+      resultArray = result.reshape(-1,).tolist()[0]
+      SMBTiles = ['S', '?', 'Q', 'E', '<', '>', '[', ']', 'o', 'B', 'b']
+      for resultElement in resultArray:
+        if resultElement in SMBTiles:
+          SMBCount+=1
+      if SMBCount > maxSMBCount:
+        maxSMBCount = SMBCount
+        maxSMBIndex = index
+      index += 1
+      SMBCount = 0
+    return results[maxSMBIndex]
+  elif mode == "maxReward":
+    index=0
+    RewardCount = 0
+    maxRewardCount = 0
+    maxRewardIndex = 0
+    for result in results:
+      resultArray = result.reshape(-1,).tolist()[0]
+      RewardTiles = ['o']
+      for resultElement in resultArray:
+        if resultElement in RewardTiles:
+          RewardCount+=1
+      if RewardCount > maxRewardCount:
+        maxRewardCount = RewardCount
+        maxRewardIndex = index
+      index += 1
+      RewardCount = 0
+    return results[maxRewardIndex]
+          
+        
+      
   
 
 def select_from_result(result, cordinates, final, mode):
@@ -111,7 +227,7 @@ for trainingCount in range(0, sampleSize):
 # initializing final output file
 final = np.empty([14,16], dtype="str")
 
-coCreativityMode = 'last'
+coCreativityMode = 'maxReward'
 # main driver code
 for subsection in range(0, number_files):
   with open("./subsections/subSection" + str(subsection) + ".txt","rt") as infile:
